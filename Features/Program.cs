cb8c5cc1800a5220393b71779da398e45034f55c
Daniel Cruz <d.afc98@gmail.com>
2021-01-27 22:53:04 -0300

    learning how to create lambda function and action

--- Features/Program.cs
+++ Features/Program.cs
@@ -8,10 +8,21 @@ namespace Features
     {
         static void Main(string[] args)
         {
+
+            Func<int, int> duplicate = x => x * x;
+            Func<int, int, int> func2 = (x, y) => x + y;
+
+            Action<string> write = x => Console.WriteLine(x);
+
+            write("TESTE");
+            Console.WriteLine(duplicate(10));
+            Console.WriteLine(func2(10, 2));
+
             IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee {Id = 1, Name = "Daniel"},
-                new Employee {Id = 2, Name = "Karina"}
+                new Employee {Id = 2, Name = "Karina"},
+                new Employee {Id = 3, Name = "Karin"}
             };
 
             IEnumerable<Employee> sales = new List<Employee>()
@@ -19,7 +30,7 @@ namespace Features
                 new Employee { Id = 3, Name = "Alex" }
             };
 
-            foreach (var employee in developers.Where(e => e.Name.StartsWith("D")))
+            foreach (var employee in developers.Where(e => e.Name.Length == 5).OrderBy(x => x.Name))
             {
                 Console.WriteLine(employee.Name);
             }
